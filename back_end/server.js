ede4d252b175989c6b43db96ae0a3f2c8a211c2b
Shun-Kiddo <mancoljayson219@gmail.com>
2025-12-28 21:23:06 +0800

    simple-authencation

---
+++ back_end/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use("/",(req,res) => {
+    res.send("Hello Worlld");
+});
+
+app.listen(process.env.PORT || 5500, 
+    console.log(`Server is running on port ${process.env.PORT|| 5500}`)
+);
